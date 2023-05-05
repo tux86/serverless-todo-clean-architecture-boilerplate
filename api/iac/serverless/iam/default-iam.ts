ae355ea0ccd4a00cb6b6898e02381074774ba125
Walid Karray <walid.karray@gmail.com>
2023-05-05 14:01:16 +0200

    add warning comment for the default iam

--- api/iac/serverless/iam/default-iam.ts
+++ api/iac/serverless/iam/default-iam.ts
@@ -1,5 +1,8 @@
 import { AWSIam } from '../../../../common/src/iac/serverless/types'
 
+// WARNING: This is not a best practice. Granting an IAM role with a wide set of permissions
+// (like allowing all actions on all resources) can lead to security vulnerabilities.
+// Instead, use the principle of least privilege and provide only necessary permissions.
 export const defaultIam: AWSIam = {
   role: {
     statements: [
@@ -11,30 +14,3 @@ export const defaultIam: AWSIam = {
     ]
   }
 }
-
-// export const defaultIam: AWSIam = {
-//   role: {
-//     statements: [
-//       // dynamodb table
-//       {
-//         Effect: 'Allow',
-//         Action: [
-//           'dynamodb:DescribeTable',
-//           'dynamodb:Query',
-//           'dynamodb:Scan',
-//           'dynamodb:GetItem',
-//           'dynamodb:PutItem',
-//           'dynamodb:UpdateItem',
-//           'dynamodb:DeleteItem'
-//         ],
-//         Resource: '*'
-//       },
-//       // cognito user pool
-//       {
-//         Effect: 'Allow',
-//         Action: ['cognito-idp:*'],
-//         Resource: '${param:userPoolArn}'
-//       }
-//     ]
-//   }
-// }
